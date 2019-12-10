6f0a4c310a072a26319d0e9e3bc7cc8110e24468
Robert Oikle <6431161+roikle@users.noreply.github.com>
2019-12-10 11:48:28 -0500

    Updated variable name from line to docid

--- nunaliit_generate_dump_by_docid_command/nunaliit_generate_dump_by_docid_command.sh
+++ nunaliit_generate_dump_by_docid_command/nunaliit_generate_dump_by_docid_command.sh
@@ -6,9 +6,9 @@ DIRECTORY=`date +"%Y-%m-%dT%T"`
 OUTPUT+="nunaliit dump --dump-dir dump/$DIRECTORY "
 
 # Read Doc Id and append --doc-dir <doc-id> string to output 
-while read line; do
+while read docid; do
 # Read next doc id from input file
-OUTPUT+="--doc-id $line "
+OUTPUT+="--doc-id $docid "
 done < $INPUTFILE
 
 # Output full command
