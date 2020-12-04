0863bb93abdd7a98b8b146aedfce51890e7190b5
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-12-04 13:24:38 -0500

    Initial commit of open building footprint download bash script

---
+++ download_open_building_footprints/download_open_building_footprints.sh
@@ -0,0 +1,42 @@
+# Download and Unzip all Microsoft Building Footprint Data
+echo Downloading Microsoft Building Footprint Data
+mkdir MS
+cd MS
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/Alberta.zip
+unzip Alberta.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/BritishColumbia.zip
+unzip BritishColumbia.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/Manitoba.zip
+unzip Manitoba.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/NewBrunswick.zip
+unzip NewBrunswick.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/NewfoundlandAndLabrador.zip
+unzip NewfoundlandAndLabrador.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/NorthwestTerritories.zip
+unzip NorthwestTerritories.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/NovaScotia.zip
+unzip NovaScotia.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/Nunavut.zip
+unzip Nunavut.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/Ontario.zip
+unzip Ontario.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/PrinceEdwardIsland.zip
+unzip PrinceEdwardIsland.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/Quebec.zip
+unzip Quebec.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/Saskatchewan.zip
+unzip Saskatchewan.zip
+
+wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/YukonTerritory.zip
+unzip YukonTerritory.zip
