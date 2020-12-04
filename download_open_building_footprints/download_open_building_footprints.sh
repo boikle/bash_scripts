646638b261eac77bfeb321039abc931e985e6180
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-12-04 13:45:17 -0500

    Added download links for OSM data

--- download_open_building_footprints/download_open_building_footprints.sh
+++ download_open_building_footprints/download_open_building_footprints.sh
@@ -1,7 +1,8 @@
 # Download and Unzip all Microsoft Building Footprint Data
 echo Downloading Microsoft Building Footprint Data
-mkdir MS
-cd MS
+mkdir ms
+cd ms
+
 wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/Alberta.zip
 unzip Alberta.zip
 
@@ -40,3 +41,47 @@ unzip Saskatchewan.zip
 
 wget https://usbuildingdata.blob.core.windows.net/canadian-buildings-v2/YukonTerritory.zip
 unzip YukonTerritory.zip
+
+# Download and Unzip all Open Street Maps Open Building Footprint Data
+echo Downloading Open Street Maps Building Footprint Data from GeoFabriks
+mkdir ../osm
+cd ../osm
+
+wget http://download.geofabrik.de/north-america/canada/alberta-latest-free.shp.zip
+unzip alberta-latest-free.zip
+
+wget http://download.geofabrik.de/north-america/canada/british-columbia-latest-free.shp.zip
+unzip british-columbia-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/manitoba-latest-free.shp.zip
+unzip manitoba-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/new-brunswick-latest-free.shp.zip
+unzip new-brunswick-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/newfoundland-and-labrador-latest-free.shp.zip
+unzip newfoundland-and-labrador-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/northwest-territories-latest-free.shp.zip
+unzip northwest-territories-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/nova-scotia-latest-free.shp.zip
+unzip nova-scotia-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/nunavut-latest-free.shp.zip
+unzip nunavut-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/ontario-latest-free.shp.zip
+unzip ontario-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/prince-edward-island-latest-free.shp.zip
+unzip prince-edward-island-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/quebec-latest-free.shp.zip
+unzip quebec-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/saskatchewan-latest-free.shp.zip
+unzip saskatchewan-latest-free.shp.zip
+
+wget http://download.geofabrik.de/north-america/canada/yukon-latest-free.shp.zip
+unzip yukon-latest-free.shp.zip
